8fa4a9aee973ce2a37a2e6a041fca11c520b34d9
bollambhavesh4-coder <bollambhavesh4@gmail.com>
2026-05-17 14:56:32 +0530

    Reduce image change interval from 3s to 2s

--- script.js
+++ script.js
@@ -102,4 +102,4 @@ const sliderImage = document.getElementById("slider-image");
 setInterval(() => {
     currentImage = (currentImage + 1) % images.length;
     sliderImage.src = images[currentImage];
-}, 3000); // Change every 3 seconds
+}, 2000); // Change every 2 seconds
