df045bf22ca40c39e6955b9a8ee72e6189719217
bollambhavesh4-coder <bollambhavesh4@gmail.com>
2026-05-17 15:16:57 +0530

    Increase image change interval to 2 seconds

--- script.js
+++ script.js
@@ -102,4 +102,4 @@ const sliderImage = document.getElementById("slider-image");
 setInterval(() => {
     currentImage = (currentImage + 1) % images.length;
     sliderImage.src = images[currentImage];
-}, 1500); // Change every 1.5 seconds
+}, 2000); // Change every 2 seconds
